b90601863ab61d0137973ff751a5d09e6eaa1800
AndresAlmarcha <412414@tecnicatura.frc.utn.edu.ar>
2026-02-02 16:35:38 -0300

    update

---
+++ ParcialWebApi/DTOs/InsertarCriptoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ParcialWebApi.DTOs
+{
+    public class InsertarCriptoDto
+    {
+        public string Nombre { get; set; }
+        public string Simbolo { get; set; }
+        public double ValorActual { get; set; }
+        public DateTime UltimaActualizacion { get; set; }
+        public int Categoria { get; set; }
+        public string Estado { get; set; }
+    }
+}
